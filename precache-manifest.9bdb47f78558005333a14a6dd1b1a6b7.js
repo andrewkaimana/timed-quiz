a6a3b12412910dc2ad95cdf6aac3914ffff8ab5d
shanromac <srmcadams@yahoo.com>
2020-03-04 11:16:28 -0500

    Updates

---
+++ precache-manifest.9bdb47f78558005333a14a6dd1b1a6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fdd8812b87b7ab8222394c1cd00c71",
+    "url": "/timed-quiz/index.html"
+  },
+  {
+    "revision": "f8b9ceaf71aecd86e381",
+    "url": "/timed-quiz/static/css/main.2bd91bfc.chunk.css"
+  },
+  {
+    "revision": "9294a6578daa51d68192",
+    "url": "/timed-quiz/static/js/2.a3e79266.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/timed-quiz/static/js/2.a3e79266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b9ceaf71aecd86e381",
+    "url": "/timed-quiz/static/js/main.b8fd05ba.chunk.js"
+  },
+  {
+    "revision": "e3b6fc8a4b2281cf344f",
+    "url": "/timed-quiz/static/js/runtime-main.c4300dc4.js"
+  }
+]);
